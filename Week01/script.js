cc587d0609b631a704df00cf07853c137c5acfc9
Brian Smith <smithbh129@gmail.com>
2022-04-21 19:04:58 -0600

    variable name fixes

--- Week01/script.js
+++ Week01/script.js
@@ -1,20 +1,20 @@
 function loadStory(){
 
-    let storyName = document.querySelector('#story-name').value;
-    let storyMarkup = localStorage.getItem(storyName);
+    let storyName = document.querySelector('#name_input').value
+    let storyHTML = localStorage.getItem(storyName)
 
-    document.querySelector('#story-editor').value = storyMarkup;
+    document.querySelector('#story_editor').value = storyHTML
 }
 function saveStory(){
 
-    let storyName = document.querySelector('#story-name').value;
-    let storyMarkup = document.querySelector('#story-editor').value;
+    let storyName = document.querySelector('#story_input').value
+    let storyHTML = document.querySelector('#story_editor').value
 
-    localStorage.setItem(storyName, storyMarkup);
+    localStorage.setItem(storyName, storyHTML)
 }
 function displayStory(){
 
-    let storyMarkup = document.querySelector('#story-editor').value;
+    let storyHTML = document.querySelector('#story_editor').value
 
-    document.querySelector('#story-display').innerHTML = storyMarkup;
+    document.querySelector('#story_display').innerHTML = storyHTML
 }
