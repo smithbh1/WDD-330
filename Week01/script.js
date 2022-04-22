5c637da3ae55152c3588c492cd7c5cb0a3a7993d
Brian Smith <smithbh129@gmail.com>
2022-04-21 18:36:05 -0600

    notes and story final

---
+++ Week01/script.js
@@ -0,0 +1,20 @@
+function loadStory(){
+
+    let storyName = document.querySelector('#story-name').value;
+    let storyMarkup = localStorage.getItem(storyName);
+
+    document.querySelector('#story-editor').value = storyMarkup;
+}
+function saveStory(){
+
+    let storyName = document.querySelector('#story-name').value;
+    let storyMarkup = localStorage.getItem(storyName); 
+
+    localStorage.setItem(storyName, storyMarkup);
+}
+function displayStory(){
+
+    let storyMarkup = document.querySelector('#story-editor').value;
+
+    document.querySelector('#story-display').innerHTML = storyMarkup;
+}
